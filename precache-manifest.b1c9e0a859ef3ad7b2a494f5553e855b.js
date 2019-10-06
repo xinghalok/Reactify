d479a66bc808e25efb632e3056e2deb2fcf7b83f
akshat0047 <pande.akshat21@gmail.com>
2019-10-06 12:07:02 +0530

    Updates

---
+++ precache-manifest.b1c9e0a859ef3ad7b2a494f5553e855b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5517573d93813cf2aec069728a83d322",
+    "url": "/Reactify/index.html"
+  },
+  {
+    "revision": "8d464f8ec333f508e905",
+    "url": "/Reactify/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "004e33618e4360d730b5",
+    "url": "/Reactify/static/css/main.60028c2d.chunk.css"
+  },
+  {
+    "revision": "8d464f8ec333f508e905",
+    "url": "/Reactify/static/js/2.f034cafe.chunk.js"
+  },
+  {
+    "revision": "004e33618e4360d730b5",
+    "url": "/Reactify/static/js/main.a40d28b8.chunk.js"
+  },
+  {
+    "revision": "460acd0edd19095e767a",
+    "url": "/Reactify/static/js/runtime-main.63371d2d.js"
+  },
+  {
+    "revision": "e26673cc785ecd96ad951010b182acbb",
+    "url": "/Reactify/static/media/dp.e26673cc.jpeg"
+  },
+  {
+    "revision": "43042451d52505a1e14aeb9df4f3da2a",
+    "url": "/Reactify/static/media/logo.43042451.png"
+  }
+]);
